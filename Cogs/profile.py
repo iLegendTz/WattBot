c9f5cf0adc5d5862e58377da9c6dcf73f610a8d0
iLegendTz <miguel.isaac@live.com>
2022-03-04 17:09:51 -0600

    El comando para visualizar el avatar ahora es funcional

---
+++ Cogs/profile.py
@@ -0,0 +1,40 @@
+from discord.ext import commands
+from discord import Embed
+from discord.ext.commands import Context
+
+from main import bot
+
+
+class Profile(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Profile Cog')
+
+    @commands.command(name="avatar")
+    async def build(self, ctx: Context, user_id: str = None):
+        if not user_id:
+            user = await bot.fetch_user(user_id=ctx.author.id)
+        else:
+            user = await bot.fetch_user(removeExtraCharactersFromUserId(user_id=user_id))
+
+        embed = Embed(title="{0}#{1}".format(user.name, user.discriminator))
+        embed.set_image(url=user.avatar_url)
+
+        await ctx.reply(embed=embed)
+
+
+def removeExtraCharactersFromUserId(user_id: str) -> int:
+    characters_to_remove = "<!@>"
+
+    for character in characters_to_remove:
+        user_id = user_id.replace(character, "")
+
+    return user_id
+
+
+def setup(client):
+    client.add_cog(Profile(client))
