19a19b5a8702a4e8011d4fb415a47604c92a1ab8
iLegendTz <miguel.isaac@live.com>
2022-03-07 05:39:55 -0600

    La variable bot ahora es client

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ from discord.ext import commands
 
 load_dotenv()
 
-bot = commands.Bot(command_prefix=">")
+client = commands.Bot(command_prefix=">")
 
 
-@bot.event
+@client.event
 async def on_ready():
     print('Wattbot esta ready <3')
 
 for filename in os.listdir('./Cogs'):
     if filename.endswith('.py'):
-        bot.load_extension(f'Cogs.{filename[:-3]}')
+        client.load_extension(f'Cogs.{filename[:-3]}')
 
-bot.run(os.getenv("TOKEN"))
+client.run(os.getenv("TOKEN"))
